9c26a838abb0c4d4172e6b6a9c2cefaf091acda1
IhorBoichuk <ihorboichuk74@gmail.com>
2024-01-30 15:09:04 +0000

    'fix_last'

--- Proton_mail.py
+++ Proton_mail.py
@@ -18,54 +18,25 @@ def send_proton_email(email_to, email_subject, email_message):
     driver.find_element(By.ID, 'password').send_keys('ij+)+R)NqrY@Mkhl')
     sleep(2)
     driver.find_element(By.XPATH, "/html/body/div[1]/div[4]/div[1]/main/div[1]/div[2]/form/button").click()
-
     sleep(10)
     driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/div/div[2]/div/div[1]/div[2]/button").click()
     sleep(10)
-
-    # print(dir(driver))
     driver.switch_to.active_element.send_keys(email_to)
-    driver.find_element(By.CSS_SELECTOR, '[id^="subject-composer-"]').send_keys('test')
-    el = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div[1]')
-    driver.execute_script("arguments[0].textContent = 'Your New Text';", el)
-
-    # driver.find_element(By.CSS_SELECTOR, '/html/body/div[1]/div[4]/div/div/div/footer/div/div[1]/button[1]/span').click()
-    # driver.find_element(By.CSS_SELECTOR, '/html/body/div[1]/div[4]/div/div/div/footer/div/div[1]/button[1]/span').click()
-    sleep(3)
-    # pyautogui.typewrite('\t\t\n')
-    # sleep(10)
-    
-    # driver.switch_to.active_element.send_keys(email_subject)
-    # pyautogui.typewrite('\t\t')
-    # sleep(10)
-    # input_el = driver.find_element(By.XPATH, '/html/body/div[1]/div/div[1]/div[1]')
-    # driver.execute_script(f"arguments[0].textContent = '{email_message}'", input_el)
+    driver.find_element(By.CSS_SELECTOR, '[id^="subject-composer-"]').send_keys(email_subject)
+    sleep(5)
+    pyautogui.typewrite('\t\t')
+    driver.switch_to.active_element.send_keys(email_message)
     sleep(10)
     driver.find_element(By.XPATH, '/html/body/div[1]/div[4]/div/div/div/footer/div/div[1]/button[1]/span').click()
     sleep(5)
-
-# //*[@id="to-composer-2268"]
-# //*[@id="subject-composer-2125"]
-
-
-
-
-    # driver.find_element(By.XPATH, '//*[@id="pm_sidebar"]/section/a').click()
-    # sleep(2)
-    # driver.switch_to_active_element().send_keys(email_to + '\n' + '\t' + email_subject + '\t')
-    # sleep(0.5)
-    # driver.switch_to_active_element().send_keys(email_message + '\t' + '\t' + '\t' + '\t' + '\t' + '\t')
-    # sleep(0.5)
-    # driver.switch_to_active_element().click()
-    
-    # driver.quit()
+    driver.quit()
     # display.stop()
-    # print('E-mail Sent!')
-    # del email_subject
-    # del email_message
-    # del driver
+    print('E-mail Sent!')
+    del email_subject
+    del email_message
+    del driver
     # del display
  
 
-send_proton_email('receiver_email@gmail.com', 'test', 'testmsg')
+send_proton_email('ranokpvp@gmail.com', 'test', 'testmsg')
 #TEST
